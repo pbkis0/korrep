417eb5d1cd2ed6a24d959045fdab00d3bd7770f9
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-06-12 08:33:46 +0200

    Elkészült a sör modell
    Get, és set, konstruktor-t elkészült.

--- C_Sharp/MVC/Model/Beer.cs
+++ C_Sharp/MVC/Model/Beer.cs
@@ -1,12 +1,122 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MVC.Model
 {
     public class Beer
     {
+        #region Adattagok
+
+        private int azonosito;
+        private string marka;
+        private double alkoholTartalom;
+        private int ar;
+        private string gyarto;
+
+        #endregion
+
+        #region Konstruktorok
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="azonosito"></param>
+        /// <param name="marka"></param>
+        /// <param name="alkoholTartalom"></param>
+        /// <param name="ar"></param>
+        /// <param name="gyarto"></param>
+        public Beer(int azonosito, string marka, double alkoholTartalom, int ar, string gyarto)
+        {
+            this.azonosito = azonosito;
+            this.marka = marka;
+            this.alkoholTartalom = alkoholTartalom;
+            this.ar = ar;
+            this.gyarto = gyarto;
+        }
+
+        /// <summary>
+        /// Üres konstruktor
+        /// </summary>
+        public Beer()
+        {
+
+        }
+
+        #endregion
+
+        #region Getter és setter
+
+        public int GetAzonosito()
+        {
+            return azonosito;
+        }
+
+        public void setAzonosito(int azonosito)
+        {
+            this.azonosito = azonosito;
+        }
+
+        public string GetMarka()
+        {
+            return marka;
+        }
+
+        public void setMarka(string marka)
+        {
+            this.marka = marka;
+        }
+
+        /// <summary>
+        /// alkoholtartalom
+        /// </summary>
+        /// <returns></returns>
+        public double getAlkoholTartalom()
+        {
+            return alkoholTartalom;
+        }
+
+        public void setAlkolTartalom(double alkoholTartalom)
+        {
+            this.alkoholTartalom = alkoholTartalom;
+        }
+
+        //Ár
+        public int getAr()
+        {
+            return ar;
+        }
+
+        public void setAr(int ar)
+        {
+            this.ar = ar;
+        }
+
+        //Gyártó
+        public string getGyarto()
+        {
+            return gyarto;
+        }
+
+        public void setGyarto(string gyarto)
+        {
+            this.gyarto = gyarto;
+        }
+
+        #endregion
+
+        #region toString
+
+        public override string ToString()
+        {
+            return azonosito + " " + marka + " " + alkoholTartalom + " " + ar + " " + gyarto;
+            //return $"{azonosito} {marka} {alkoholTartalom} {ar} {gyarto}";
+        }
+
+        #endregion
     }
 }
