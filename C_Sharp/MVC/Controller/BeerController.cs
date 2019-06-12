bebd99cca765e8c727b0eed929fff452a1a2d3b7
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-06-12 08:49:56 +0200

    fájlbeolvasás.

--- C_Sharp/MVC/Controller/BeerController.cs
+++ C_Sharp/MVC/Controller/BeerController.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVC.Model;
+using System.IO;
 
 namespace MVC.Controller
 {
     public class BeerController
     {
+        private List<Beer> sorok;
+
+        public BeerController()
+        {
+            sorok = new List<Beer>();
+        }
+
+        public void beolvasSoroket()
+        {
+            // var: fordítás időben kitalálja a változó típusát
+
+            var beolvasas = File.ReadAllText("../../Datasources/beer.txt", Encoding.UTF8);
+        }
     }
 }
