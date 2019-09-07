318e86c51ba961e71f5f74dde841c06e2bd50b50
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2019-09-07 20:33:21 +0200

    csv hozzaadva

--- kozponti_vizsga/Helsinki2017/Helsinki2017/Helsinki2017/Program.cs
+++ kozponti_vizsga/Helsinki2017/Helsinki2017/Helsinki2017/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Helsinki2017
 {
     class Program
     {
+        static List<Versenyzo> versenyzok;
+
+        static void FileBeolvasas()
+        {
+            var sorok = File.ReadAllLines("../../", Encoding.UTF8).ToList();
+
+            foreach (var sor in sorok)
+            {
+                var adat = sor.Split(';');
+
+                string nev = adat[0];
+                string orszag = adat[1];
+                string technikai = adat[2];
+                string komponens = adat[3];
+
+                Versenyzo v = new Versenyzo()
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");x
