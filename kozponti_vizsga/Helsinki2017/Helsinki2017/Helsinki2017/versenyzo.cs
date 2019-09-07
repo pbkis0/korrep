d6d22506af9159d22dd156f366d844711d7005db
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2019-09-07 19:16:06 +0200

    versenyzo.cs

--- kozponti_vizsga/Helsinki2017/Helsinki2017/Helsinki2017/versenyzo.cs
+++ kozponti_vizsga/Helsinki2017/Helsinki2017/Helsinki2017/versenyzo.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace Helsinki2017
 {
-    class versenyzo
+    public class Versenyzo
     {
+        public string Nev { get; set; }
+
+        public string Orszag { get; set; }
+
+        public string Techinikai { get; set; }
+
+        public string Komponens { get; set; }
+
+        public List<int> levonas { get; set; }
+
+        public Versenyzo(string nev, string orszag, string technikai, string komponens, List<int> levonas)
+        {
+            Nev = new;
+            Orszag = orszag;
+            Techinikai = technikai;
+            Komponens = komponens;
+            this.levonas = levonas;
+            //Kivülről jövő lista, egyenlő a belső listával.
+            //A kintről jövő adatot adja értékül a listának.
+        }
+
+        public override string ToString()
+        {
+            return Nev + " " + Orszag + " " + Techinikai + " " + Komponens;
+
+        }
+
     }
 }
