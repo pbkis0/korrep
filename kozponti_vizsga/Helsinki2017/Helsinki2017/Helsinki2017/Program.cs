907a16b9081f4aafb79550dc7eb70d64355aa0cb
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-09-08 10:31:44 +0200

    Fileok beolvasása.

--- kozponti_vizsga/Helsinki2017/Helsinki2017/Helsinki2017/Program.cs
+++ kozponti_vizsga/Helsinki2017/Helsinki2017/Helsinki2017/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Helsinki2017
 {
     class Program
     {
-        static List<Versenyzo> versenyzok;
+        static List<Versenyzo> rovidProgram;
+        static List<Versenyzo> donto;
 
-        static void FileBeolvasas()
+        static void FileBeolvasasRovidProgram()
+        {
+            var sorok = File.ReadAllLines("../../rovidprogram.csv", Encoding.UTF8).ToList();
+
+            foreach (var sor in sorok)
+            {
+                var adat = sor.Split(';');
+
+                string nev = adat[0];
+                string orszag = adat[1];
+                double technikai =Convert.ToDouble(adat[2]);
+                double komponens = Convert.ToDouble(adat[3]);
+                int levonas = Convert.ToInt32(adat[4]);
+
+                Versenyzo v = new Versenyzo(nev, orszag, technikai, komponens, levonas);
+                rovidProgram.Add(v);
+            }
+        }
+
+        static void FileBeolvasasDonto()
         {
             var sorok = File.ReadAllLines("../../donto.csv", Encoding.UTF8).ToList();
 
@@ -18,19 +40,19 @@ namespace Helsinki2017
 
                 string nev = adat[0];
                 string orszag = adat[1];
-                string technikai = adat[2];
-                string komponens = adat[3];
+                double technikai = Convert.ToDouble(adat[2]);
+                double komponens = Convert.ToDouble(adat[3]);
+                int levonas = Convert.ToInt32(adat[4]);
 
-                Versenyzo v = new Versenyzo(nev, orszag, technikai, komponens);
-                versenyzok.Add(v);
+                Versenyzo v = new Versenyzo(nev, orszag, technikai, komponens, levonas);
+                donto.Add(v);
             }
-        }
 
         static void Main(string[] args)
         {
             versenyzok = new List<Versenyzo>();
             FileBeolvasas();
-            Console.WriteLine("Hello World!");x
+            Console.WriteLine("Hello World!");
         }
     }
 }
