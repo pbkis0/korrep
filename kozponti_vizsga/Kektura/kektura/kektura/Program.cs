9555ada26ca7edac2f27e2ad81a248bc45c5a2e7
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2019-09-15 09:56:00 +0200

    File Beolvasas proba

--- kozponti_vizsga/Kektura/kektura/kektura/Program.cs
+++ kozponti_vizsga/Kektura/kektura/kektura/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace kektura
 {
     class Program
     {
+        static List<versenyzo> versenyzok;
+
+        static void FileBeolvasas()
+        {
+            var sor = File.ReadAllLines("../../kektura.csv", Encoding.UTF8).ToList();
+
+            foreach (var sor in sor)
+            {
+                var adat = sor.Split(';');
+
+                string turaKiinduloPont = adat[0];
+                string turaVegPont = adat[1];
+                double turaszakaszHossz = adat[2];
+                double emelkedesOsszege = adat[3];
+                double lejtesekOsszege = adat[4];
+                bool pecsetelohely = adat[5];
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
