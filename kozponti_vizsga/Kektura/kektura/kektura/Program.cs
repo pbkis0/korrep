48de9fefa455c5a5641fc80b1ce15453f584a3e4
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2019-09-15 09:45:32 +0200

    kektura adatok beolvasasa

---
+++ kozponti_vizsga/Kektura/kektura/kektura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kektura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ kozponti_vizsga/Kektura/kektura/kektura/versenyzo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kektura
+{
+    public class versenyzo
+    {
+        public string TuraKiinduloPont { get; set; }
+
+        public string TuraVegPont { get; set; }
+
+        public double TuraszakaszHossza { get; set; }
+
+        public double EmelkedesOsszege { get; set; }
+
+        public double LejtesekOsszege { get; set; }
+
+        public bool Pecsetelohely { get; set; }
+
+        public versenyzo(string turaKiinduloPont, string turaVegPont, double turaszakaszHossz, double emelkedesOsszege, double lejtesekOsszege, bool pecsetelohely)
+        {
+            TuraKiinduloPont = turaKiinduloPont;
+            TuraVegPont = turaVegPont;
+            TuraszakaszHossza = turaszakaszHossz;
+            EmelkedesOsszege = emelkedesOsszege;
+            LejtesekOsszege = lejtesekOsszege;
+            Pecsetelohely = pecsetelohely;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
