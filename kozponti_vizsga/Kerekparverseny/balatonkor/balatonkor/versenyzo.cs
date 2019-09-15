261dbef5d1eb423465950666295b2c5b6403c4bf
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2019-09-15 10:13:23 +0200

    balatonkor adatok beolvasasa

---
+++ kozponti_vizsga/Kerekparverseny/balatonkor/balatonkor/versenyzo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balatonkor
+{
+    public class Versenyzo
+    {
+        public string Nev { get; set; }
+
+        public double ElsoSzakasz { get; set; }
+
+        public double MasodikSzakasz { get; set; }
+
+        public double HarmadikSzakasz { get; set; }
+
+        public Versenyzo(string nev, double elsoSzakasz, double masodikSzakasz, double harmadikSzakasz)
+        {
+            Nev = nev;
+            ElsoSzakasz = elsoSzakasz;
+            MasodikSzakasz = masodikSzakasz;
+            HarmadikSzakasz = harmadikSzakasz;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
