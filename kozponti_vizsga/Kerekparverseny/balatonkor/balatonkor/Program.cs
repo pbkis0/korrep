ce3919eac034921085578d98e94080734ff3c6d1
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2019-09-15 10:22:06 +0200

    Update Program.cs

--- kozponti_vizsga/Kerekparverseny/balatonkor/balatonkor/Program.cs
+++ kozponti_vizsga/Kerekparverseny/balatonkor/balatonkor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,24 @@ using System.Threading.Tasks;
 namespace balatonkor
 {
     class Program
+
+        static List<Versenyzo> versenyzok;
+
+    static void FileBeolvasas()
     {
+        var sorok = File.ReadAllLines("../../kerekpar.csv", Encoding.UTF8).ToList();
+
+        foreach (var sor in sorok)
+        {
+            var adat = sor.Split(';');
+
+            string nev = adat[0];
+            double elszoSzakasz = adat[1];
+            double masodikSzakasz = adat[2];
+            double harmadikSzakasz = adat[3];
+        }
+    }
+
         static void Main(string[] args)
         {
         }
