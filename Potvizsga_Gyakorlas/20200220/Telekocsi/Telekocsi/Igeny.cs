74d9a9d07e8467d764b0b90707a85c81537f3101
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2020-02-20 18:15:57 +0100

    Telekocsi

---
+++ Potvizsga_Gyakorlas/20200220/Telekocsi/Telekocsi/Igeny.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi
+{
+    public class Igeny
+    {
+        public string azonosito { get; set; }
+        public string indulas { get; set; }
+        public string cel { get; set; }
+        public int szemelyek { get; set; }
+
+        public override string ToString()
+        {
+            return azonosito + " " + indulas + " " + cel + " " + szemelyek;
+        }
+
+    }
+}
---
+++ Potvizsga_Gyakorlas/20200220/Telekocsi/Telekocsi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Telekocsi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+    }
+}
