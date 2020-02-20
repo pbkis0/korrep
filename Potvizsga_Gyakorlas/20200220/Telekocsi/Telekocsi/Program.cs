cd10e6290e67247c249207c6ccb8c064634ef3c6
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2020-02-20 19:33:34 +0100

    Telekocsi feladat elkeszult

--- Potvizsga_Gyakorlas/20200220/Telekocsi/Telekocsi/Program.cs
+++ Potvizsga_Gyakorlas/20200220/Telekocsi/Telekocsi/Program.cs
@@ -47,15 +47,77 @@ namespace Telekocsi
 
             }
 
+            /*2. feladat */
+            Console.WriteLine($"2. feladat: {Autok.Count} autós hirdetett fuvart.");
 
+            /*3.feladat */
+
+            /*Megszámlásás tétele */
+            int db = 0;
+
+            foreach (var a in Autok)
+            {
+                if (a.indulas == "Budapest" && a.cel == "Miskolc")
+                {
+                    db += 1;  // db++
+                }
+            }
+            Console.WriteLine($"3. Feladat: Összesen {db} férőhelyet hirdettek az autósok Budapestről Miskolcra");
+
+            /*4. feladat */
+            /*Maximum keresés tétele */
+
+            int max = 0;
+            foreach (var a in Autok)
+            {
+                if (max < a.ferohely)
+                {
+                    max = a.ferohely;
+                }
+            }
+
+            foreach (var a in Autok)
+            {
+                if (a.ferohely == max)
+                {
+                    Console.WriteLine($"A legtöbb férőhelyeket ({max}) a {a.indulas} - {a.cel} útvonalon ajánlották fel a hirdetők");
+                }
+            }
+
+            /*5. feladat */
+
+            foreach (var igeny in Igenyek)
+            {
+                foreach (var auto in Autok)
+                {
+                    if (igeny.indulas == auto.indulas && igeny.cel == auto.cel && igeny.szemelyek <= auto.ferohely)
+                    {
+                        Console.WriteLine($"{igeny.azonosito} => {auto.rendszam}");
+                    }
+                }
+            }
+
+            /*6. feladat */
+
+            string fajlba = "";
+
+            foreach (var igeny in Igenyek)
+            {
+                foreach (var auto in Autok)
+                {
+                    if (igeny.indulas == auto.indulas && igeny.cel == auto.cel && igeny.szemelyek <= auto.ferohely)
+                    {
+                        fajlba += $"{igeny.azonosito}: Rendszám: {auto.rendszam}, Telefonszam: {auto.telefonszam}\n";
+                    }
+                    else
+                    {
+                        fajlba += $"{igeny.azonosito}: Sajnos nem sikerült autót találni.\n";
+                    }
+                }
+            }
+            File.WriteAllText("../../utasuzenetek.txt", fajlba, Encoding.UTF8);
 
             Console.ReadKey();
-
-
-
-
-
-
         }
     }
 }
