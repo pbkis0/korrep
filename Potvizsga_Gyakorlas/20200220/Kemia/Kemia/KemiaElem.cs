cfe1ae148e23face69eaf73d5891f796fbf456ba
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2020-02-20 20:49:00 +0100

    Kémiai elemek

---
+++ Potvizsga_Gyakorlas/20200220/Kemia/Kemia/KemiaElem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemia
+{
+    public class KemiaElem
+    {
+        public string ev { get; set; }
+        public string elem { get; set; }
+        public string vegyjel { get; set; }
+        public int rendszam { get; set; }
+        public string felfedezo { get; set; }
+
+        public override string ToString()
+        {
+            return ev + " " + elem + " " + vegyjel + " " + rendszam + " " + felfedezo;
+        }
+    }
+}
---
+++ Potvizsga_Gyakorlas/20200220/Kemia/Kemia/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Kemia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<KemiaElem> elemek = new List<KemiaElem>();
+
+            var sorok = File.ReadAllLines("../../felfedezesek.csv", Encoding.UTF8).Skip(1).ToList();
+
+            foreach (var sor in sorok)
+            {
+                var adatok = sor.Split(';');
+                KemiaElem e = new KemiaElem();
+                e.ev = adatok[0];
+                e.elem = adatok[1];
+                e.vegyjel = adatok[2];
+                e.rendszam = Convert.ToInt32(adatok[3]);
+                e.felfedezo = adatok[4];
+
+                elemek.Add(e);
+    
+            }
+
+            /*3.Feladat */
+            /*Lista elemeinek összeg */
+            Console.WriteLine($"3.feladat: Elemek száma: {elemek.Count} ");
+
+            /*4.feladat */
+            /*Összegzés tétele */
+
+            int okordb = 0;
+
+            foreach (var e in elemek)
+            {
+                if (e.ev == "Ókor")
+                {
+                    okordb++;
+                }
+
+            }
+            Console.WriteLine($"4. Feladat: Felfedezések száma az Ókorban: {okordb}");
+
+            ///*5. feladat */
+            //var minta = @"[a-zA-Z]";
+            //string vegyjel = "";
+            //do
+            //{
+            //    Console.WriteLine("Kérek egy vegyjelet: ");
+            //    vegyjel = Console.ReadLine();
+            //} while (vegyjel.Length > 2 || vegyjel.Length <= 0 || !Regex.IsMatch(vegyjel, minta));
+
+            /*6. feladat */
+            string vegyjel = "Sg";
+
+            foreach (var e in elemek)
+            {
+                if (e.vegyjel == vegyjel)
+                {
+                    Console.WriteLine(e.elem);
+                }
+            }
+
+            /* 7. feladat*/
+            int minEv = int.MaxValue;
+            int maxEv = int.MinValue;
+
+            /*Minimum, maximum szűrés */
+            for (int i = 9; i < elemek.Count; i++)
+            {
+                if (minEv > Convert.ToInt32(elemek[i].ev))
+                {
+                    minEv = Convert.ToInt32(elemek[i].ev);
+                }
+            }
+
+            for (int i = 9; i < elemek.Count; i++)
+            {
+                if (maxEv < Convert.ToInt32(elemek[i].ev))
+                {
+                    maxEv = Convert.ToInt32(elemek[i].ev);
+                }
+            }
+
+            Console.WriteLine(maxEv-minEv);
+
+            Console.ReadKey();
+        }
+    }
+}
