f1428f776f94ca36fd8bde2f3113206afc9bfd6a
pbkis0 <43212190+pbkis0@users.noreply.github.com>
2020-05-03 12:05:56 +0200

    2019 Noveberi Kozponti Irasfeli feladat

---
+++ Potvizsga_Gyakorlas/20200503/2019November_Kozponti_Irasbeli/OpeningWeekend/OpeningWeekend/NyitoHetvege.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpeningWeekend
+{
+    public class NyitoHetvege
+    {
+        public string eredetiCim { get; set; }
+        public string magyarCim { get; set; }
+        public string date { get; set; }
+        public string forgalmazo { get; set; }
+        public double bevel { get; set; }
+        public int latogato { get; set; }
+
+        public override string ToString()
+        {
+            return $"{eredetiCim} {magyarCim} {date} {forgalmazo} {bevel} {latogato}";
+        }
+    }
+}
